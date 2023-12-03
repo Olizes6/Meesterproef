c2e0e7ceaef6a12f77fa5eedca6ce01d7e06431a
Olizes6 <olizes@outlook.com>
2023-12-03 22:14:02 +0100

    Ran some test runs, improved filter initialization

--- MNIST.py
+++ MNIST.py
@@ -18,16 +18,22 @@ mnist_labels = np.asarray(mnist_labels, dtype=np.int32)
 data_train, data_test, labels_train, labels_test = train_test_split(
     mnist_data, mnist_labels, test_size=10000, random_state=42)
 
-data_train = data_train / 255.0
+# Dividing the data into a subset
+data_train = data_train[:40000] / 255.0
 data_train = data_train.reshape(-1, 28, 28)
 
-fig, (ax1, ax2) = plt.subplots(nrows = 2, ncols=1)
+data_test = data_test[:10000] / 255.0
+data_test = data_test.reshape(-1, 28, 28)
 
-resume_training = True
+labels_train = labels_train[:40000]
+
+labels_test = labels_test[:10000] 
+
+resume_training = False
 start_epoch = 0
 
-checkpoint_dir = 'checkpoints/'
-selected_checkpoint = "checkpoints/model_parameters_epoch_2.pkl"
+checkpoint_dir = 'checkpoints_ontwerpcyclus_3.1/'
+selected_checkpoint = (f"{checkpoint_dir}model_parameters_epoch_4.pkl")
 
 # Check if the directory exists; if not, create it
 if not os.path.exists(checkpoint_dir):
@@ -45,8 +51,9 @@ class Convolution:
         self.filter_shape = (num_filters, filter_size, filter_size)  # (3,3)
         self.output_shape = (num_filters, input_height -
                              filter_size + 1, input_width - filter_size + 1)
+       
         self.filters = np.random.randn(
-            *self.filter_shape) * np.sqrt(2 / (input_height * input_width * 1))  # 1 for the amount of channels because MNIST images are greyscale images
+            *self.filter_shape) * np.sqrt(2 / np.prod(self.filter_shape[1:]))
         self.biases = np.zeros(self.output_shape)
 
     def forward(self, input_data):
@@ -159,7 +166,7 @@ class Fully_Connected:
         self.input_size = input_size
         self.output_size = output_size
         self.weights = np.random.randn(output_size, input_size)
-        self.biases = np.random.randn(output_size, 1)
+        self.biases = np.zeros(output_size).reshape(-1, 1)
 
     def forward(self, input_data):
         self.input_data = input_data
@@ -197,64 +204,82 @@ class Fully_Connected:
 
 
 def cross_entropy_loss(y_true, y_pred):
-    num_samples = y_true.shape[0]
+    num_samples = len(y_true)
 
     epsilon = 1e-7
     y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
-    loss = -np.sum(y_true * np.log(y_pred + epsilon)) / num_samples
+    loss = -np.sum(y_true * np.log(y_pred)) / num_samples
     return loss
 
 
 def cross_entropy_loss_gradient(y_true, y_pred):
-    num_samples = y_true.shape[0]
-    epsilon = 1e-7
-    gradient = -y_true / (y_pred + epsilon) / num_samples
-
+    num_samples = len(y_true)
+    gradient = -y_true / (y_pred + 1e-7) / num_samples
     return gradient
 
-def plot_data(loss, accuracy, epochs):
-   print(epochs, accuracy, loss)
-   ax1.plot(epochs, accuracy, color="#FFA500")
-   ax2.plot(epochs, loss, color="#0096FF")
+def plot_data(train_loss, train_accuracy, val_loss, val_accuracy, epochs):
+    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
+    epochs = range(1, epochs + 1)
+    ax1.plot(epochs, train_accuracy, label='Training Accuracy', color="#FFA500", marker=".")
+    ax1.plot(epochs, val_accuracy, label='Validation Accuracy', color="#FF0000", marker=".")
+    ax1.set_title("Model Accuracy")
+    ax1.set_ylabel("Accuracy")
+    ax1.set_xlabel("Epoch")
+    ax1.legend()
 
-   ax1.set_title("Model Accuracy")
-   ax1.set_ylabel("Accuracy")
-   ax1.set_xlabel("Epoch")
+    ax2.plot(epochs, train_loss, label='Training Loss', color="#0096FF", marker=".")
+    ax2.plot(epochs, val_loss, label='Validation Loss', color="#FF0000", marker=".")
+    ax2.set_title("Model Loss")
+    ax2.set_ylabel("Loss")
+    ax2.set_xlabel("Epoch")
+    ax2.legend()
 
-   ax2.set_title("Model Loss")
-   ax2.set_ylabel("Loss")
-   ax2.set_xlabel("Epoch")
-
-   plt.tight_layout()
-   plt.show()
+    plt.tight_layout()
+    plt.show()
 
 
 conv = Convolution(data_train[0].shape, 3, 32)
 pool = MaxPool(2)
 full = Fully_Connected(5408, 10)
+accuracy_data = []  
+loss_data = [] 
+val_accuracy_data = []
+val_loss_data = []
+epoch_data = []
 
+# Load selected checkpoint
 if resume_training and os.path.exists(selected_checkpoint):
     with open(selected_checkpoint, "rb") as file:
         loaded_checkpoint = pickle.load(file)
 
     conv.set_parameters(loaded_checkpoint["conv"])
     full.set_parameters(loaded_checkpoint["full"])
+    accuracy_data = loaded_checkpoint['accuracy_data']
+    loss_data = loaded_checkpoint['loss_data']
+    val_accuracy_data = loaded_checkpoint['val_accuracy_data']
+    val_loss_data = loaded_checkpoint['val_loss_data']
     start_epoch = loaded_checkpoint["epoch"]
-    print("Loaded checkpoint: ", loaded_checkpoint)
+    print(loaded_checkpoint)
 
-def train_network(X_train, y_train, conv, pool, full, learning_rate=0.01, epochs=24):
+# with open(selected_checkpoint, 'rb') as file:
+#     loaded_checkpoint = pickle.load(file)
+#     print(loaded_checkpoint["loss_data"], loaded_checkpoint["accuracy_data"], loaded_checkpoint["val_loss_data"], loaded_checkpoint["val_accuracy_data"], loaded_checkpoint["epoch"])
+#     plot_data(loaded_checkpoint["loss_data"], loaded_checkpoint["accuracy_data"], loaded_checkpoint["val_loss_data"], loaded_checkpoint["val_accuracy_data"], loaded_checkpoint["epoch"])
+
+def train_network(X_train, y_train, X_val, y_val, conv, pool, full, learning_rate=0.01, epochs=24):
     start_time = time.time()
-    accuracy_graph = []  # To store accuracy values for each epoch
-    loss_graph = []  # To store loss values for each epoch
     initial_learning_rate = learning_rate
-    epoch_graph = []
+    minimum_learning_rate = 1e-5
+
     for epoch in range(start_epoch, epochs):
         total_loss = 0.0
         correct_predictions = 0
+
         # Reduce learning rate to reach convergence faster
-        new_learning_rate = (1 / (1 + 1.5 * epoch)) * initial_learning_rate
+        new_learning_rate = max(minimum_learning_rate, ((1 / (1 + 1 * epoch)) * initial_learning_rate))
         learning_rate = new_learning_rate 
         print("current learning rate: ", new_learning_rate)
+
         for i in range(len(X_train)):
 
             # Forward propagation
@@ -263,7 +288,6 @@ def train_network(X_train, y_train, conv, pool, full, learning_rate=0.01, epochs
             full_out = full.forward(pool_out)
 
             # Convert the scalar label to one-hot encoding
-            # Assuming there are 10 classes in MNIST
             actual_label_one_hot = np.zeros(10)
             actual_label_one_hot[y_train[i]] = 1
 
@@ -274,9 +298,7 @@ def train_network(X_train, y_train, conv, pool, full, learning_rate=0.01, epochs
             one_hot_pred[np.argmax(full_out)] = 1
             one_hot_pred = one_hot_pred.flatten()
 
-            # Calculate the accuracy of the predictions for this batch
             num_pred = np.argmax(one_hot_pred)
-           
             correct_predictions += np.sum(num_pred == y_train[i])
 
             if (i + 1) % 500 == 0:
@@ -286,20 +308,49 @@ def train_network(X_train, y_train, conv, pool, full, learning_rate=0.01, epochs
             full_back = full.backward(gradient, learning_rate)
             pool_back = pool.backward(full_back)
             conv_back = conv.backward(pool_back, learning_rate)
-        end_time = time.time()
-        epoch_time = end_time - start_time
 
-        # Calculate the average loss for this epoch
+        # Calculate the average training loss for this epoch
         average_loss = total_loss / len(X_train)
 
-        # Calculate the accuracy for this epoch
+        # Calculate the training accuracy for this epoch
         accuracy = correct_predictions / len(X_train) * 100
-        print(f"Epoch {epoch + 1}/{epochs} - Loss: {average_loss:.4f} - Accuracy: {accuracy:.2f}% execution time: {epoch_time:.2f} seconds")
-        # Append the accuracy and loss values to their respective lists
-        accuracy_graph.append(accuracy)
-        loss_graph.append(average_loss)
-        epoch_graph.append(epoch)
-        #plot_data(loss_graph, accuracy_graph, epoch_graph)
+
+        val_total_loss = 0.0
+        val_correct_predictions = 0
+
+        for i in range(len(X_val)):
+            conv_out = conv.forward(X_val[i])
+            pool_out = pool.forward(conv_out)
+            full_out = full.forward(pool_out)
+
+            # Convert the scalar label to one-hot encoding
+            actual_label_one_hot = np.zeros(10)
+            actual_label_one_hot[y_val[i]] = 1
+
+            loss = cross_entropy_loss(actual_label_one_hot, full_out.flatten())
+            val_total_loss += loss
+
+            one_hot_pred = np.zeros_like(full_out.flatten())
+            one_hot_pred[np.argmax(full_out)] = 1
+            one_hot_pred = one_hot_pred.flatten()
+
+            num_pred = np.argmax(one_hot_pred)
+            val_correct_predictions += np.sum(num_pred == y_val[i])
+
+        # Calculate validation set accuracy and loss
+        val_average_loss = val_total_loss / len(X_val)
+        val_accuracy = val_correct_predictions / len(X_val) * 100
+
+        # Print and store results
+        print(f"Epoch {epoch + 1}/{epochs} - "
+              f"Training Loss: {average_loss:.4f} - Training Accuracy: {accuracy:.2f}% - "
+              f"Validation Loss: {val_average_loss:.4f} - Validation Accuracy: {val_accuracy:.2f}%")
+        
+        accuracy_data.append(accuracy)
+        loss_data.append(average_loss)
+        val_accuracy_data.append(val_accuracy)
+        val_loss_data.append(val_average_loss)
+        epoch_data.append(epoch)
 
         # Save model parameters
         checkpoint_filename = f'{checkpoint_dir}model_parameters_epoch_{epoch + 1}.pkl'
@@ -310,7 +361,11 @@ def train_network(X_train, y_train, conv, pool, full, learning_rate=0.01, epochs
             pickle.dump({
                 "epoch": epoch + 1,
                 "conv": conv_parameters, 
-                "full": full_parameters
+                "full": full_parameters,
+                "accuracy_data": accuracy_data,
+                "loss_data": loss_data,
+                "val_accuracy_data": val_accuracy_data,
+                "val_loss_data": val_loss_data,
             }, file)
             
     end_time = time.time()
@@ -318,30 +373,10 @@ def train_network(X_train, y_train, conv, pool, full, learning_rate=0.01, epochs
     print(f"Total execution time: {total_execution_time:.2f} seconds")
 
     # Print final epoch statistics
-    print(f"Final Accuracy: {accuracy:.2f}%, Final Loss: {average_loss:.4f}")
+    print(f"Final Training Accuracy: {accuracy:.2f}%, Final Training Loss: {average_loss:.4f}")
+    print(f"Final Validation Accuracy: {val_accuracy:.2f}%, Final Validation Loss: {val_average_loss:.4f}")
 
+    # Call this function after training is complete
+    plot_data(loss_data, accuracy_data, val_loss_data, val_accuracy_data, epoch_data)
 
-def predict(input_sample, conv, pool, full):
-    # Forward propagation through convolution and pooling
-    conv_out = conv.forward(input_sample)
-    pool_out = pool.forward(conv_out)
-    # Flattening
-    flattened_output = pool_out.flatten()
-    # Forward propagation through fully connected layer
-    predictions = full.forward(flattened_output)
-    return predictions
-
-
-train_network(data_train, labels_train, conv, pool, full)
-
-predictions = []
-
-for data in data_test:
-    pred = predict(data, conv, pool, full)
-    one_hot_pred = np.zeros_like(pred)
-    one_hot_pred[np.argmax(pred)] = 1
-    predictions.append(one_hot_pred.flatten())
-
-predictions = np.array(predictions)
-
-print(predictions)
+train_network(data_train, labels_train, data_test, labels_test, conv, pool, full)
